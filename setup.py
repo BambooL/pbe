17481d512aefe52fe3a382328a37766b414a7ed5
BambooL <bambool@psu.edu>
2018-03-13 22:41:21 -0700

    setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='pbe',
+      version='0.1',
+      description='Programming by Example',
+      url='http://github.com/BambooL/pbe',
+      author='Xiao Liu',
+      author_email='bambool@psu.edu',
+      install_requires=[
+          'setuptools',
+          'deap'
+      ],
+      zip_safe=False)
